ddda71a93059fde69879b45641c67ec0a0ceba3b
nick <64551534+null-nick@users.noreply.github.com>
2025-08-30 00:00:58 +0200

    indent fix

--- app/src/main/java/org/thunderdog/challegram/ui/SettingsBugController.java
+++ app/src/main/java/org/thunderdog/challegram/ui/SettingsBugController.java
@@ -749,12 +749,12 @@ public class SettingsBugController extends RecyclerViewController<SettingsBugCon
       case Section.EXPERIMENTS: {
 
         if (Config.EDGE_TO_EDGE_CUSTOMIZABLE) {
-            if (!items.isEmpty()) {
-                items.add(new ListItem(ListItem.TYPE_SHADOW_TOP));
-            }
-            items.add(new ListItem(ListItem.TYPE_RADIO_SETTING, R.id.btn_experiment, 0, R.string.Experiment_NoEdgeToEdge).setLongValue(Settings.EXPERIMENT_FLAG_NO_EDGE_TO_EDGE));
-            items.add(new ListItem(ListItem.TYPE_SHADOW_BOTTOM));
-            items.add(new ListItem(ListItem.TYPE_DESCRIPTION, 0, 0, R.string.Experiment_NoEdgeToEdgeInfo));
+          if (!items.isEmpty()) {
+            items.add(new ListItem(ListItem.TYPE_SHADOW_TOP));
+          }
+          items.add(new ListItem(ListItem.TYPE_RADIO_SETTING, R.id.btn_experiment, 0, R.string.Experiment_NoEdgeToEdge).setLongValue(Settings.EXPERIMENT_FLAG_NO_EDGE_TO_EDGE));
+          items.add(new ListItem(ListItem.TYPE_SHADOW_BOTTOM));
+          items.add(new ListItem(ListItem.TYPE_DESCRIPTION, 0, 0, R.string.Experiment_NoEdgeToEdgeInfo));
         }
 
         items.add(new ListItem(ListItem.TYPE_RADIO_SETTING, R.id.btn_experiment, 0, R.string.Experiment_PeerIds).setLongValue(Settings.EXPERIMENT_FLAG_SHOW_PEER_IDS));
@@ -766,7 +766,7 @@ public class SettingsBugController extends RecyclerViewController<SettingsBugCon
         items.add(new ListItem(ListItem.TYPE_DESCRIPTION, 0, 0, Lang.getMarkdownStringSecure(this, R.string.Experiment_ResolutionInfo)));
 
         if (items.isEmpty()) {
-            items.add(new ListItem(ListItem.TYPE_EMPTY, 0, 0, R.string.ExperimentalSettingsUnavailable));
+          items.add(new ListItem(ListItem.TYPE_EMPTY, 0, 0, R.string.ExperimentalSettingsUnavailable));
         }
 
         break;
