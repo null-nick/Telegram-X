3f0a04be77729a11a9aaf0e550d86cecb71e1a65
nick. <64551534+null-nick@users.noreply.github.com>
2026-09-24 21:42:03 +0200

    Keep bitmap size limit for large single photos in chat bubbles

--- app/src/main/java/org/thunderdog/challegram/data/MediaWrapper.java
+++ app/src/main/java/org/thunderdog/challegram/data/MediaWrapper.java
@@ -991,6 +991,9 @@ public class MediaWrapper implements FileProgressComponent.SimpleListener, FileP
 
   public boolean setImageScaling (int size) {
     size = Math.min(MAX_BITMAP_SIZE, size);
+    if (size == 0 && targetSize != null && Math.max(targetSize.width, targetSize.height) > MAX_BITMAP_SIZE) {
+      size = MAX_BITMAP_SIZE;
+    }
     if (targetImageFile != null && targetImageFile.getSize() != size) {
       targetImageFile.setSize(size);
       return showImage();
